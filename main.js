ee71b1a1ec20f86d641086c1246115f89a44c76f
Andrew Barnes <andrewjbarnes94@gmail.com>
2024-05-07 20:57:21 -0400

    React Frontend started

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
-const createWindow = () => {
-    const win = new BrowserWindow({
-      width: 800,
-      height: 600
-    })
-  
-    win.loadFile('index.html')
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  win.loadFile('src/index.html');
 }
 
-app.whenReady().then(() => {
-    createWindow()
-})
+app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-})
-
+  if (process.platform !== 'darwin') app.quit();
+});
